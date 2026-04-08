b32ed468e4151e145421bd703f5cd64a21e8d6ba
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-04-08 11:58:48 +0200

    Added chopping interaction

--- player/InventoryData.cs
+++ player/InventoryData.cs
@@ -3,28 +3,28 @@ using System;
 
 public partial class InventoryData : Node
 {
-    public static int ItemId { get; set; } = -1; // -1 = 空
-    public static int Count { get; set; } = 0;
+	public static int ItemId { get; set; } = -1; // -1 = 空
+	public static int Count { get; set; } = 0;
 
-    // add items
-    public static void AddItem(int id, int amount = 1)
-    {
+	// add items
+	public static void AddItem(int id, int amount = 1)
+	{
 
-        if (Count == 0)
-        {
-            ItemId = id;
-            Count = amount;
-        }
-        else if (ItemId == id)
-        {
-            Count += amount;
-        }
-    }
+		if (Count == 0)
+		{
+			ItemId = id;
+			Count = amount;
+		}
+		else if (ItemId == id)
+		{
+			Count += amount;
+		}
+	}
 
-    // clear inventory
-    public static void Clear()
-    {
-        ItemId = -1;
-        Count = 0;
-    }
+	// clear inventory
+	public static void Clear()
+	{
+		ItemId = -1;
+		Count = 0;
+	}
 }
