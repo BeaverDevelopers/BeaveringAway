187822747af329b8749a8d023da26f25a3f638a1
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-05-18 21:50:02 +0200

    changed item data for inventory drop
    added a filepath in item data to be able to instantiate item scene when dropping

--- player/ItemData.cs
+++ player/ItemData.cs
@@ -7,4 +7,5 @@ public partial class ItemData : Resource
     [Export] public Texture2D Icon;
     [Export] public int ItemCount = 1;
     [Export] public Godot.Collections.Array<string> itemRecipe = new();
+    [Export] public string ItemScenePath;
 }
