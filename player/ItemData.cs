52c5b584e8ce345a21364806a41589a56de01b98
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-05-18 21:43:28 +0200

    Revert "Items can be placed into the world from inventory"

--- player/ItemData.cs
+++ player/ItemData.cs
@@ -7,6 +7,4 @@ public partial class ItemData : Resource
     [Export] public Texture2D Icon;
     [Export] public int ItemCount = 1;
     [Export] public Godot.Collections.Array<string> itemRecipe = new();
-
-    [Export] public string ItemScenePath;
 }
