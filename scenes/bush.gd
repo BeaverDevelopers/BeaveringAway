d3d5976094545c8a405a4e2e6af19adb4c4db2b4
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-05-27 09:58:14 +0200

    Updated bush.gd to not spawn too many berries

--- scenes/bush.gd
+++ scenes/bush.gd
@@ -8,6 +8,7 @@ extends StaticBody2D
 
 static var rng = RandomNumberGenerator.new()
 var is_alive = false
+var berry_added = false
 
 func _ready() -> void:
 	# Setup interaction and default tree pose
@@ -26,31 +27,33 @@ func set_alive(alive: bool):
 	if alive:
 		dry_bush.hide()
 		good_bush.show()
-		#trying to prevent overlapping berries
-		var y_coordinates = [randi_range(-10, 10), randi_range(-10, 10), randi_range(-10, 10), randi_range(-10, 10)]
-		y_coordinates.sort()
-		for i in range(1, y_coordinates.size()):
-			if y_coordinates[i] - y_coordinates[i-1] < 5:
-				if y_coordinates[i] <= 5:
-					y_coordinates[i] = y_coordinates[i-1] + 5
-				else:
-					y_coordinates[i] += 3
-					y_coordinates[i-1] -= 2
+		if not berry_added:
+				##trying to prevent overlapping berries
+			#var y_coordinates = [randi_range(-10, 10), randi_range(-10, 10), randi_range(-10, 10), randi_range(-10, 10)]
+			#y_coordinates.sort()
+			#for i in range(1, y_coordinates.size()):
+				#if y_coordinates[i] - y_coordinates[i-1] < 5:
+					#if y_coordinates[i] <= 5:
+						#y_coordinates[i] = y_coordinates[i-1] + 5
+					#else:
+						#y_coordinates[i] += 3
+						#y_coordinates[i-1] -= 2
+			
+			for i in range(3):
+				var dropped_item = berry.instantiate()
+				dropped_item.ItemData = berry_drop
+				if i == 0:
+					dropped_item.global_position = good_bush.global_position + Vector2(randi_range(-5, -12), randi_range(5, 7))
+				if i == 1:
+					dropped_item.global_position = good_bush.global_position + Vector2(randi_range(-4, 4), randi_range(-4, -8))
+				if i == 2:
+					dropped_item.global_position = good_bush.global_position + Vector2(randi_range(5, 12), randi_range(5, 7))
+				#var x = randi_range(-15, 13)
+				#dropped_item.global_position = good_bush.global_position + Vector2(x, y_coordinates[i])
+				dropped_item.z_index = 10 #because otherwise they sometimes disappeared 
+				get_parent().add_child(dropped_item)
 				
-		
-		for i in range(4):
-			var dropped_item = berry.instantiate()
-			dropped_item.ItemData = berry_drop
-			#if i == 0:
-				#dropped_item.global_position = good_bush.global_position + Vector2(-15, -10)
-			#if i == 1:
-				#dropped_item.global_position = good_bush.global_position + Vector2(-5, -10)
-			#if i == 2:
-				#dropped_item.global_position = good_bush.global_position + Vector2(-15, 10)
-			var x = randi_range(-15, 13)
-			dropped_item.global_position = good_bush.global_position + Vector2(x, y_coordinates[i])
-			dropped_item.z_index = 1 #because otherwise they sometimes disappeared 
-			get_parent().add_child(dropped_item)
+			berry_added = true
 	else:
 		good_bush.hide()
 		dry_bush.show()
